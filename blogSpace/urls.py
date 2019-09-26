182f525dcf04ceb342ad35f47cb13b4f01258285
arwildo <arwildo@gmail.com>
2019-09-26 15:46:31 +0800

    add anti brute force attack

---
+++ blogSpace/urls.py
@@ -0,0 +1,30 @@
+"""blogSpace URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from captcha_admin import admin
+from django.urls import path, include
+from django.conf.urls import include, url
+from django.views.generic import TemplateView
+
+admin.autodiscover()
+
+urlpatterns = [
+    path('fight-club1999', admin.site.urls),
+    path('', include('blog.urls')),
+]
+
+urlpatterns += [
+    url(r'^robots\.txt$', TemplateView.as_view(template_name="blogSpace/robots.txt", content_type='text/plain')),
+]
