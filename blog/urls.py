52ef950cda0eff14153e2e124a197fc7835e0120
arwildo <arwildo@gmail.com>
2019-10-12 15:51:47 +0800

    add policy page

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
-from . import views
+from .views import PostList, PostDetail, Policy
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('', PostList.as_view(), name='home'),
+    path('policy/', Policy.as_view(), name='policy'),
+    path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from .models import Post
+from django.views.generic import TemplateView
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -9,3 +10,6 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class Policy(TemplateView):
+    template_name = 'policy.html'
