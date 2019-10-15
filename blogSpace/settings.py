55eaf9170681750d66218da34430b4aa7132c0a8
arwildo <arwildo@gmail.com>
2019-10-15 15:00:24 +0800

    fix scrollToTop btn hover on mobile

--- blogSpace/settings.py
+++ blogSpace/settings.py
@@ -25,8 +25,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = key() 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ADMIN_ENABLED  = False
+DEBUG = True
+ADMIN_ENABLED  = True
 
 ALLOWED_HOSTS = ['blog.arwildo.space', 'django-env.mehwk33cpp.us-west-2.elasticbeanstalk.com', '127.0.0.1']
 
