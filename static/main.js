128ff0d123c21db24442e0e3b4690cc679883dae
arwildo <arwildo@gmail.com>
2019-10-15 14:47:25 +0800

    add animation openScrollBtn

--- static/main.js
+++ static/main.js
@@ -7,12 +7,14 @@ gtag('config', 'UA-51659765-2');
 
 
 // Scroll To Top
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 60px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-        document.getElementById("scrollToTop").style.display = "block";
+        openScrollBtn(){
+            document.getElementById("scrollToTop").style.display = "block";
+        }
     } else {
         document.getElementById("scrollToTop").style.display = "none";
     }
