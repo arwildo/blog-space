9731b6c42080517c9622727c01a8a8c91aef83c5
arwildo <arwildo@gmail.com>
2019-11-03 23:44:05 +0800

    change 404 style

--- static/main.js
+++ static/main.js
@@ -7,7 +7,6 @@ gtag('config', 'UA-51659765-2');
 
 
 // Scroll To Top
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -19,7 +18,6 @@ function scrollFunction() {
    
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
  
      $('html, body').animate({scrollTop:0}, 'slow');
